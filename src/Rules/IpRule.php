1530d0ea488214dc9ae4f3e22b2233be23b91431
党留辉 <liuhuidang@sf-express.com>
2019-05-29 11:47:06 +0800

    支持判断ip是否在某个范围内

--- src/Rules/IpRule.php
+++ src/Rules/IpRule.php
@@ -22,8 +22,21 @@ class IpRule extends RuleAbstract
      */
     public function check($value)
     {
+        // 判断是否为IP
         if (filter_var($value, FILTER_VALIDATE_IP) === false) {
             return false;
+        }
+        // 判断ip是否在某个范围内
+        if (isset($this->params['start']) && !empty($this->params['start'])) {
+            if (ip2long($this->params['start']) > ip2long($value)) {
+                return false;
+            }
+        }
+
+        if (isset($this->params['end']) && !empty($this->params['end'])) {
+            if (ip2long($this->params['end']) < ip2long($value)) {
+                return false;
+            }
         }
         return ["data" => $value];
     }
